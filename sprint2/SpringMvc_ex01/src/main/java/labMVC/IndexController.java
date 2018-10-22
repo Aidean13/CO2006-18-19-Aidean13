fa791321fd10e7b310eeaf0102657fecebfd9993
Aidean13 <ac682@student.le.ac.uk>
2018-10-22 13:02:29 +0100

    exo2 solved but did clear a few syntax errors with the solution such as {} around values

--- sprint2/SpringMvc_ex01/src/main/java/labMVC/IndexController.java
+++ sprint2/SpringMvc_ex01/src/main/java/labMVC/IndexController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class IndexController {
 	
-	// ADD YOUR REQUEST MAPPING AND METHOD HERE
-	
+	@RequestMapping("/hello")
+	 public String hello(Model model) {
+	      model.addAttribute("name", "World");
+	      return "hello";
+	  }   
 }
